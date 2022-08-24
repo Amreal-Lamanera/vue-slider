7c8e587b485271f784bd6ea7a811c1631d1cc273
Amreal-Lamanera <pieraccini.francesco@gmail.com>
2022-08-24 14:49:21 +0200

    first commit

---
+++ js/slider.js
@@ -0,0 +1,28 @@
+// console.log('slider')
+
+const slides = [
+	'./img/01.jpg',
+	'./img/02.jpg',
+	'./img/03.jpg',
+	'./img/04.jpg',
+	'./img/05.jpg',
+]
+
+const app = new Vue({
+	el: '#app',
+	data: {
+		slides,
+		active: 0,
+		alt: 'Immagine ' + this.active
+	},
+	methods: {
+		prevSlide() {
+			if (!this.active) this.active = this.slides.length - 1
+			else this.active--
+		},
+		nextSlide() {
+			if (this.active === this.slides.length - 1) this.active = 0
+			else this.active++
+		}
+	}
+})
